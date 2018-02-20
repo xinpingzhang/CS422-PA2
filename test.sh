717ce9068a1db43bc4ef6af653cfae9ac5505fd5
Kelvin Zhang <zhangxp1998@gmail.com>
2018-02-20 16:17:37 -0500

    Remove program log once test passes

--- test.sh
+++ test.sh
@@ -4,11 +4,12 @@ output=output.log
 test()
 {
 	echo $1 | ./bin/prog2_arq > $2
-	cat $output | grep -a "MAINLOOP: data given to student" | sed "$ d" | awk '{print $6}' > given.txt
-	cat $output | grep -a "TOLAYER5" | awk '{print $4}' > received.txt
+	cat $2 | grep -a "MAINLOOP: data given to student" | sed "$ d" | awk '{print $6}' > given.txt
+	cat $2 | grep -a "TOLAYER5" | awk '{print $4}' > received.txt
 	if diff given.txt received.txt; then
 		echo "OK!"
-		rm -f given.txt received.txt
+		rm given.txt received.txt
+		rm $2
 	else
 		echo "ERROR!"
 		exit 1
@@ -19,4 +20,4 @@ echo "TEST FLIP"
 test "10 0 0.3 1000 3" "flip.log"
 
 echo "TEST LOSS"
-test "10 0.1 0 1000 3" "flip.log"
+test "10 0.1 0 1000 3" "loss.log"
