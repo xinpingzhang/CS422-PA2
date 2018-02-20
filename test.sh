bb7b35e4e7bf7be0f0fbb2953b46469a49d6838a
Kelvin Zhang <zhangxp1998@gmail.com>
2018-02-20 16:06:28 -0500

    auto clean up in script

--- test.sh
+++ test.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 output=output.log
 
-echo "10 0 0.3 1000 3" | ./bin/prog2_arq > $output
-cat $output | grep -a "MAINLOOP: data given to student" | sed "$ d" | awk '{print $6}' > given.txt
-cat $output | grep -a "TOLAYER5" | awk '{print $4}' > received.txt
-diff given.txt received.txt
+test()
+{
+	echo $1 | ./bin/prog2_arq > $2
+	cat $output | grep -a "MAINLOOP: data given to student" | sed "$ d" | awk '{print $6}' > given.txt
+	cat $output | grep -a "TOLAYER5" | awk '{print $4}' > received.txt
+	if diff given.txt received.txt; then
+		echo "OK!"
+		rm -f given.txt received.txt
+	else
+		echo "ERROR!"
+		exit 1
+	fi
+}
+
+echo "TEST FLIP"
+test "10 0 0.3 1000 3" "flip.log"
